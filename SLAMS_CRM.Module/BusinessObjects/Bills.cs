2a71952cd4d89286302d02558954cafdd4e6b030
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-05-04 09:30:28 +0300

    Edited Bills

--- SLAMS_CRM.Module/BusinessObjects/Bills.cs
+++ SLAMS_CRM.Module/BusinessObjects/Bills.cs
@@ -17,10 +17,7 @@ namespace SLAMS_CRM.Module.BusinessObjects
     [DefaultClassOptions]
     [NavigationItem("Order Management")]
     [ImageName("MainMenuItem")]
-    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
-    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
-    //[Persistent("DatabaseTableName")]
-    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+
     public class Bills : BaseObject
     { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
         // Use CodeRush to create XPO classes and properties with a few keystrokes.
