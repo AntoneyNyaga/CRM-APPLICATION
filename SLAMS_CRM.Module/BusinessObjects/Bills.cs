c4de396cad2743eb65365c55a442df9bc7668038
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-05-06 00:53:15 +0300

    modified bills

--- SLAMS_CRM.Module/BusinessObjects/Bills.cs
+++ SLAMS_CRM.Module/BusinessObjects/Bills.cs
@@ -39,10 +39,6 @@ namespace SLAMS_CRM.Module.BusinessObjects
         //    set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
         //}
 
-        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
-        //public void ActionMethod() {
-        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
-        //    this.PersistentProperty = "Paid";
-        //}
+ 
     }
 }
