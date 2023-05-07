936dda9f2fac9fe99c61a720a7a5a9c08d34ab04
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-05-07 20:09:14 +0300

    modifieed

--- SLAMS_CRM.Module/BusinessObjects/Bills.cs
+++ SLAMS_CRM.Module/BusinessObjects/Bills.cs
@@ -19,7 +19,7 @@ namespace SLAMS_CRM.Module.BusinessObjects
     [ImageName("MainMenuItem")]
 
     public class Bills : BaseObject
-    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+    { 
         // Use CodeRush to create XPO classes and properties with a few keystrokes.
         // https://docs.devexpress.com/CodeRushForRoslyn/118557
         public Bills(Session session)
