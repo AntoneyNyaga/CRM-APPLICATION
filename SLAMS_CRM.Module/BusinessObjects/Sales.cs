370af6425ef895ac33d6673c025301f1f42d7d24
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-05-11 11:59:24 +0300

    Modify

--- SLAMS_CRM.Module/BusinessObjects/Sales.cs
+++ SLAMS_CRM.Module/BusinessObjects/Sales.cs
@@ -17,7 +17,7 @@ namespace SLAMS_CRM.Module.BusinessObjects
     [DefaultClassOptions]
     [NavigationItem("Order Management")]
     [ImageName("BO_Invoice")]
-    
+
     public class SalesOrder : BaseObject
     {
         public SalesOrder(Session session) : base(session) { }
@@ -37,79 +37,6 @@ namespace SLAMS_CRM.Module.BusinessObjects
         {
             get { return _orderDate; }
             set { SetPropertyValue(nameof(OrderDate), ref _orderDate, value); }
-        }
-
-        private decimal _totalAmount;
-        [PersistentAlias(nameof(CalculateTotalAmount))]
-        public decimal TotalAmount
-        {
-            get { return _totalAmount; }
-        }
-        private decimal CalculateTotalAmount
-        {
-            get { return CalculateTotal(); }
-        }
-
-        [Association("SalesOrder-SalesOrderDetails")]
-        public XPCollection<SalesOrderDetail> Details
-        {
-            get { return GetCollection<SalesOrderDetail>(nameof(Details)); }
-        }
-
-        private decimal CalculateTotal()
-        {
-            decimal total = 0m;
-            foreach (SalesOrderDetail detail in Details)
-            {
-                total += detail.LineTotal;
-            }
-            return total;
-        }
-    }
-
-    public class SalesOrderDetail : BaseObject
-    {
-        public SalesOrderDetail(Session session) : base(session) { }
-
-        private SalesOrder _salesOrder;
-        [Association("SalesOrder-SalesOrderDetails")]
-        public SalesOrder SalesOrder
-        {
-            get { return _salesOrder; }
-            set { SetPropertyValue(nameof(SalesOrder), ref _salesOrder, value); }
-        }
-
-        private string _productCode;
-        [Size(10)]
-        public string ProductCode
-        {
-            get { return _productCode; }
-            set { SetPropertyValue(nameof(ProductCode), ref _productCode, value); }
-        }
-
-        private decimal _unitPrice;
-        public decimal UnitPrice
-        {
-            get { return _unitPrice; }
-            set { SetPropertyValue(nameof(UnitPrice), ref _unitPrice, value); }
-        }
-
-        private int _quantity;
-        public int Quantity
-        {
-            get { return _quantity; }
-            set { SetPropertyValue(nameof(Quantity), ref _quantity, value); }
-        }
-
-        private decimal _lineTotal;
-        [PersistentAlias(nameof(CalculateLineTotal))]
-        public decimal LineTotal
-        {
-            get { return _lineTotal; }
-        }
-        private decimal CalculateLineTotal
-        {
-            get { return UnitPrice * Quantity; }
         }
     }
 }
